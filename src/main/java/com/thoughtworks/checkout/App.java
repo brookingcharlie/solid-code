91b9adb975a5438f881131f74b924ab98172d90b
Charlie Brooking <cbrookin@thoughtworks.com>
2015-08-16 16:13:46 +1000

    big ugly test/code for App.main()

--- src/main/java/com/thoughtworks/checkout/App.java
+++ src/main/java/com/thoughtworks/checkout/App.java
@@ -1,6 +1,28 @@
 package com.thoughtworks.checkout;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        try (
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter writer = new PrintWriter(System.out);
+        )
+        {
+            String line = reader.readLine();
+            String[] parts = line.split(",");
+            String product = parts[0];
+            BigDecimal price = new BigDecimal(parts[1]);
+            
+            writer.println("*************** RECEIPT ****************");
+            writer.println();
+            writer.println(String.format("%-32s%8.2f", product, price));
+            writer.println(String.format("%-32s%8s", "", "--------"));
+            writer.println(String.format("%-32s%8.2f", "Total for 1 items", price));
+            writer.println(String.format("%-32s%8s", "", "========"));
+        }
     }
 }
--- src/test/java/com/thoughtworks/checkout/AppTest.java
+++ src/test/java/com/thoughtworks/checkout/AppTest.java
@@ -1,10 +1,42 @@
 package com.thoughtworks.checkout;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class AppTest {
     @Test
-    public void test() {
-        App.main(new String[] {});
+    public void testSingle() throws Exception {
+        String inString =
+            "Pizza - Pepperoni,12.99";
+        String expectedOutString =
+            "*************** RECEIPT ****************\n" +
+            "\n" +
+            "Pizza - Pepperoni                  12.99\n" +
+            "                                --------\n" +
+            "Total for 1 items                  12.99\n" +
+            "                                ========\n";
+        try (
+            InputStream in = new ByteArrayInputStream(inString.getBytes("UTF-8"));
+            ByteArrayOutputStream outBytes = new ByteArrayOutputStream();
+            PrintStream out = new PrintStream(outBytes);
+        ) {
+            System.setIn(in);
+            System.setOut(out);
+            App.main(new String[] {});
+            String actualOutString = new String(outBytes.toByteArray(), "UTF-8");
+            assertThat(actualOutString, is(equalTo(expectedOutString)));
+        }
+        finally {
+            System.setIn(null);
+            System.setOut(null);
+        }
     }
 }
