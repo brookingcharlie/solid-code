d4258e1daedc126746e280365f8d41cbf3e401f9
Charlie Brooking <cbrookin@thoughtworks.com>
2015-08-17 01:37:07 +1000

    fix indenting

--- src/main/java/com/thoughtworks/checkout/BasketWriter.java
+++ src/main/java/com/thoughtworks/checkout/BasketWriter.java
@@ -6,16 +6,16 @@ import java.io.Writer;
 
 public class BasketWriter {
     public void write(AccessibleBasket basket, Writer output) throws IOException {
-      try (PrintWriter printWriter = new PrintWriter(output)) {
-          printWriter.println("*************** RECEIPT ****************");
-          printWriter.println();
-          for (int i = 0; i < basket.getCount(); i++) {
-              printWriter.println(String.format("%-32s%8.2f", basket.getItem(i).getProduct(), basket.getItem(i).getPrice()));
-          }
-          printWriter.println(String.format("%-32s%8s", "", "--------"));
-          String totalDescription = String.format("Total for %d items", basket.getCount());
-          printWriter.println(String.format("%-32s%8.2f", totalDescription, basket.getTotal()));
-          printWriter.println(String.format("%-32s%8s", "", "========"));
-      }
+        try (PrintWriter printWriter = new PrintWriter(output)) {
+            printWriter.println("*************** RECEIPT ****************");
+            printWriter.println();
+            for (int i = 0; i < basket.getCount(); i++) {
+                printWriter.println(String.format("%-32s%8.2f", basket.getItem(i).getProduct(), basket.getItem(i).getPrice()));
+            }
+            printWriter.println(String.format("%-32s%8s", "", "--------"));
+            String totalDescription = String.format("Total for %d items", basket.getCount());
+            printWriter.println(String.format("%-32s%8.2f", totalDescription, basket.getTotal()));
+            printWriter.println(String.format("%-32s%8s", "", "========"));
+        }
     }
 }
