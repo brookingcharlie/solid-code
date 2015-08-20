94b7dd8d44b3407ebb930c293308cf1802ffcbaa
Charlie Brooking <cbrookin@thoughtworks.com>
2015-08-20 23:25:45 +1000

    add WeekendSaleBasketFactoryUsingNow

---
+++ src/main/java/com/thoughtworks/checkout/WeekendSaleBasketFactoryUsingNow.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.checkout;
+
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
+
+import java.time.LocalDate;
+
+// To illustrate testing problem when using LocalDate.now()
+public class WeekendSaleBasketFactoryUsingNow implements BasketFactory {
+  @Override
+  public Basket createBasket() {
+    LocalDate date = LocalDate.now();
+    if (date.getDayOfWeek().equals(SATURDAY) || date.getDayOfWeek().equals(SUNDAY)) {
+      return new SaleBasket();
+    }
+    else {
+      return new Basket();
+    }
+  }
+}
